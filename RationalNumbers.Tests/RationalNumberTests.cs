a02b8087e9e83bb87ff346b5587ba630e910f9d8
Ivan Yakimov <edlinsoft@gmail.com>
2016-11-26 13:12:31 +0300

    Implementation of all operations

--- RationalNumbers.Tests/RationalNumberTests.cs
+++ RationalNumbers.Tests/RationalNumberTests.cs
@@ -48,7 +48,8 @@ namespace EdlinSoftware.RationalNumbers.Tests
         [InlineData(-1, 3, "-1/3")]
         [InlineData(-4, -3, "4/3")]
         [InlineData(long.MaxValue, long.MaxValue, "1")]
-        [InlineData(long.MinValue, long.MinValue, "1")]
+        [InlineData(long.MaxValue, long.MinValue + 1, "-1")]
+        [InlineData(long.MinValue + 1, long.MaxValue, "-1")]
         public void ToString_Values(long inputNumerator, long inputDenominator, string expectedString)
         {
             var rationalNumber = new RationalNumber(inputNumerator, inputDenominator);
@@ -68,6 +69,107 @@ namespace EdlinSoftware.RationalNumbers.Tests
             var rationalNumber2 = new RationalNumber(numerator2, denominator2);
 
             Assert.Equal(areEqual, rationalNumber1.Equals(rationalNumber2));
+        }
+
+        [Theory]
+        [InlineData(4, 2, 4, 2, true)]
+        [InlineData(4, 2, 2, 1, true)]
+        [InlineData(4, 2, -2, -1, true)]
+        [InlineData(4, 2, -2, 1, false)]
+        [InlineData(5, 3, 7, 2, false)]
+        public void EqualityOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2, bool areEqual)
+        {
+            var rationalNumber1 = new RationalNumber(numerator1, denominator1);
+            var rationalNumber2 = new RationalNumber(numerator2, denominator2);
+
+            Assert.Equal(areEqual, rationalNumber1 == rationalNumber2);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 4, 2, false)]
+        [InlineData(4, 2, 2, 1, false)]
+        [InlineData(4, 2, -2, -1, false)]
+        [InlineData(4, 2, -2, 1, false)]
+        [InlineData(4, 2, 6, 1, true)]
+        [InlineData(5, 3, 7, 2, true)]
+        [InlineData(long.MaxValue - 1, long.MaxValue, long.MaxValue - 1, long.MaxValue, false)]
+        [InlineData(long.MaxValue - 2, long.MaxValue, long.MaxValue - 1, long.MaxValue, true)]
+        [InlineData(long.MinValue + 2, long.MaxValue, long.MinValue + 1, long.MaxValue, false)]
+        public void LessOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2, bool isTrue)
+        {
+            var rationalNumber1 = new RationalNumber(numerator1, denominator1);
+            var rationalNumber2 = new RationalNumber(numerator2, denominator2);
+
+            Assert.Equal(isTrue, rationalNumber1 < rationalNumber2);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 4, 2, false)]
+        [InlineData(4, 2, 2, 1, false)]
+        [InlineData(4, 2, -2, -1, false)]
+        [InlineData(4, 2, -2, 1, true)]
+        [InlineData(4, 2, 6, 1, false)]
+        [InlineData(5, 3, 7, 2, false)]
+        [InlineData(7, 2, 5, 3, true)]
+        [InlineData(long.MaxValue - 1, long.MaxValue, long.MaxValue - 1, long.MaxValue, false)]
+        [InlineData(long.MaxValue - 2, long.MaxValue, long.MaxValue - 1, long.MaxValue, false)]
+        [InlineData(long.MinValue + 2, long.MaxValue, long.MinValue + 1, long.MaxValue, true)]
+        public void GreaterOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2, bool isTrue)
+        {
+            var rationalNumber1 = new RationalNumber(numerator1, denominator1);
+            var rationalNumber2 = new RationalNumber(numerator2, denominator2);
+
+            Assert.Equal(isTrue, rationalNumber1 > rationalNumber2);
+        }
+
+        [Theory]
+        [InlineData(2, 1, 2, 1, 4, 1)]
+        [InlineData(2, 1, -2, 1, 0, 1)]
+        [InlineData(2, 3, 3, 4, 17, 12)]
+        [InlineData(5, 6, 7, 8, 41, 24)]
+        public void AddOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2,
+            long resultNumerator, long resultDenominator)
+        {
+            Assert.Equal(new RationalNumber(resultNumerator, resultDenominator), new RationalNumber(numerator1, denominator1) + new RationalNumber(numerator2, denominator2));
+        }
+
+        [Theory]
+        [InlineData(2, 1, 2, 1, 0, 1)]
+        [InlineData(2, 1, -2, 1, 4, 1)]
+        [InlineData(2, 3, 3, 4, -1, 12)]
+        [InlineData(5, 6, 7, 8, -1, 24)]
+        public void SubOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2,
+            long resultNumerator, long resultDenominator)
+        {
+            Assert.Equal(new RationalNumber(resultNumerator, resultDenominator), new RationalNumber(numerator1, denominator1) - new RationalNumber(numerator2, denominator2));
+        }
+
+        [Theory]
+        [InlineData(2, 1, 2, 1, 4, 1)]
+        [InlineData(2, 1, -2, 1, -4, 1)]
+        [InlineData(2, 3, 3, 4, 1, 2)]
+        [InlineData(5, 6, 7, 8, 35, 48)]
+        public void MulOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2,
+            long resultNumerator, long resultDenominator)
+        {
+            Assert.Equal(new RationalNumber(resultNumerator, resultDenominator), new RationalNumber(numerator1, denominator1) * new RationalNumber(numerator2, denominator2));
+        }
+
+        [Theory]
+        [InlineData(2, 1, 2, 1, 1, 1)]
+        [InlineData(2, 1, -2, 1, -1, 1)]
+        [InlineData(2, 3, 3, 4, 8, 9)]
+        [InlineData(5, 6, 7, 8, 20, 21)]
+        public void DivOperator_Values(long numerator1, long denominator1, long numerator2, long denominator2,
+            long resultNumerator, long resultDenominator)
+        {
+            Assert.Equal(new RationalNumber(resultNumerator, resultDenominator), new RationalNumber(numerator1, denominator1) / new RationalNumber(numerator2, denominator2));
+        }
+
+        [Fact]
+        public void DivOperator_DivisionByZero()
+        {
+            Assert.Throws<DivideByZeroException>(() => new RationalNumber(1, 1) / new RationalNumber(0, 1));
         }
 
     }
--- RationalNumbers/RationalNumber.cs
+++ RationalNumbers/RationalNumber.cs
@@ -23,9 +23,12 @@ namespace EdlinSoftware.RationalNumbers
         /// <param name="numerator">Value of numerator.</param>
         /// <param name="denominator">Value of denominator.</param>
         /// <exception cref="System.ArgumentOutOfRangeException">Denominator is zero.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">Denominator or numerator is <see cref="long.MinValue"/>.</exception>
         public RationalNumber(long numerator, long denominator)
         {
             if (denominator == 0L) throw new ArgumentOutOfRangeException(nameof(denominator));
+            if (denominator == long.MinValue) throw new ArgumentOutOfRangeException(nameof(denominator));
+            if (numerator == long.MinValue) throw new ArgumentOutOfRangeException(nameof(numerator));
 
             if (numerator == 0)
             {
@@ -101,6 +104,266 @@ namespace EdlinSoftware.RationalNumbers
             { return Numerator.ToString(); }
 
             return Numerator + "/" + Denominator;
+        }
+
+        public static bool operator ==(RationalNumber rn1, RationalNumber rn2)
+        {
+            return rn1.Equals(rn2);
+        }
+
+        public static bool operator !=(RationalNumber rn1, RationalNumber rn2)
+        {
+            return !(rn1 == rn2);
+        }
+
+        public static bool operator <(RationalNumber rn1, RationalNumber rn2)
+        {
+            if (rn1.Numerator < 0 && rn2.Numerator >= 0)
+            { return true; }
+            if (rn1.Numerator >= 0 && rn2.Numerator < 0)
+            { return false; }
+
+            bool inverse = rn1.Numerator < 0 && rn2.Numerator < 0;
+
+            var n1 = rn1.Numerator;
+            var d1 = rn1.Denominator;
+            var n2 = rn2.Numerator;
+            var d2 = rn2.Denominator;
+            if (inverse)
+            {
+                n1 = -n1;
+                n2 = -n2;
+            }
+
+            return inverse ? !IsLess(n1, d2, n2, d1) : IsLess(n1, d2, n2, d1);
+        }
+
+        /// <summary>
+        /// Checks if <paramref name="a"/>*<paramref name="b"/> is less than <paramref name="c"/>*<paramref name="d"/>.
+        /// </summary>
+        private static bool IsLess(long a, long b, long c, long d)
+        {
+            // Solve: (a*b < c*d) 
+
+            // a = a0 + n*a1
+            // b = b0 + n*b1
+            // c = c0 + n*c1
+            // d = d0 + n*d1
+            // n = 2^32
+
+            ulong a0 = (ulong)a & 0xFFFFFFFF;
+            ulong a1 = (ulong)a >> 32;
+            ulong b0 = (ulong)b & 0xFFFFFFFF;
+            ulong b1 = (ulong)b >> 32;
+            ulong c0 = (ulong)c & 0xFFFFFFFF;
+            ulong c1 = (ulong)c >> 32;
+            ulong d0 = (ulong)d & 0xFFFFFFFF;
+            ulong d1 = (ulong)d >> 32;
+
+            // Solve: a0*b0 + (a0*b1 + a1*b0)*n + a1*b1*n*n < c0*d0 +(c0*d1 + c1*d0)*n + c1*d1*n*n
+            UInt64 x00 = a0 * b0;
+            UInt64 x01 = c0 * d0;
+
+            // this one may overflow, so split them up even more:
+            ulong x10 = (a0 * b1) & 1 + (a1 * b0) & 1; // lower bit
+            ulong x11 = (c0 * d1) & 1 + (c1 * d0) & 1; // lower bit
+            ulong x20 = a0 * b1 / 2 + a1 * b0 / 2; // top 63 bits
+            ulong x21 = c0 * d1 / 2 + c1 * d0 / 2; // top 63 bits
+            if (x10 == 2)
+            {
+                x20++;
+                x10 = 0;
+            }
+            if (x11 == 2)
+            {
+                x21++;
+                x11 = 0;
+            }
+
+            UInt64 x30 = a1 * b1;
+            UInt64 x31 = c1 * d1;
+
+            if (x30 < x31)
+                return true;
+            else if (x30 > x31)
+                return false;
+            else if (x20 < x21)
+                return true;
+            else if (x20 > x21)
+                return false;
+            else if (x10 < x11)
+                return true;
+            else if (x10 > x11)
+                return false;
+            else if (x00 < x01)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >(RationalNumber rn1, RationalNumber rn2)
+        {
+            return !(rn1 == rn2) && !(rn1 < rn2);
+        }
+
+        public static bool operator <=(RationalNumber rn1, RationalNumber rn2)
+        {
+            return (rn1 == rn2) || (rn1 < rn2);
+        }
+
+        public static bool operator >=(RationalNumber rn1, RationalNumber rn2)
+        {
+            return (rn1 == rn2) || (rn1 > rn2);
+        }
+
+        public static RationalNumber operator -(RationalNumber rn)
+        {
+            return new RationalNumber(-rn.Numerator, rn.Denominator);
+        }
+
+        public static RationalNumber operator +(RationalNumber rn1, RationalNumber rn2)
+        {
+            // (a*b + c*d)/(e*f) = (n1*d2 + n2*d1)/(d1*d2)
+
+            var a = rn1.Numerator;
+            var b = rn2.Denominator;
+            var c = rn2.Numerator;
+            var d = rn1.Denominator;
+            var e = rn1.Denominator;
+            var f = rn2.Denominator;
+
+            var gcd = GreatestCommonDenominator(e, f);
+            if (gcd != 1)
+            {
+                b /= gcd;
+                d /= gcd;
+                e /= gcd;
+            }
+
+            return new RationalNumber(a * b + c * d, e * f);
+        }
+
+        public static RationalNumber operator -(RationalNumber rn1, RationalNumber rn2)
+        {
+            return rn1 + (-rn2);
+        }
+
+        public static RationalNumber operator *(RationalNumber rn1, RationalNumber rn2)
+        {
+            var n1 = rn1.Numerator;
+            var d1 = rn1.Denominator;
+            var n2 = rn2.Numerator;
+            var d2 = rn2.Denominator;
+
+            var gcd = GreatestCommonDenominator(n1, d2);
+            if (gcd != 1)
+            {
+                n1 /= gcd;
+                d2 /= gcd;
+            }
+
+            gcd = GreatestCommonDenominator(n2, d1);
+            if (gcd != 1)
+            {
+                n2 /= gcd;
+                d1 /= gcd;
+            }
+
+            return new RationalNumber(n1 * n2, d1 * d2);
+        }
+
+        public static RationalNumber operator /(RationalNumber rn1, RationalNumber rn2)
+        {
+            if(rn2.Numerator == 0)
+                throw new DivideByZeroException();
+
+            var n1 = rn1.Numerator;
+            var d1 = rn1.Denominator;
+            var n2 = rn2.Numerator;
+            var d2 = rn2.Denominator;
+
+            var gcd = GreatestCommonDenominator(n1, n2);
+            if (gcd != 1)
+            {
+                n1 /= gcd;
+                n2 /= gcd;
+            }
+
+            gcd = GreatestCommonDenominator(d1, d2);
+            if (gcd != 1)
+            {
+                d1 /= gcd;
+                d2 /= gcd;
+            }
+
+            return new RationalNumber(n1 * d2, d1 * n2);
+        }
+
+        public static implicit operator RationalNumber(byte value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(sbyte value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(Int16 value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(UInt16 value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(Int32 value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(UInt32 value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(Int64 value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        public static implicit operator RationalNumber(UInt64 value)
+        {
+            if (value > long.MaxValue)
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            return new RationalNumber((long)value, 1);
+        }
+
+        public static explicit operator double(RationalNumber value)
+        {
+            var n = (double)value.Numerator;
+            var d = (double)value.Denominator;
+
+            return n / d;
+        }
+
+        public static explicit operator decimal(RationalNumber value)
+        {
+            var n = (decimal)value.Numerator;
+            var d = (decimal)value.Denominator;
+
+            return n / d;
+        }
+
+        public static explicit operator float(RationalNumber value)
+        {
+            var n = (float)value.Numerator;
+            var d = (float)value.Denominator;
+
+            return n / d;
         }
     }
 }
